291c98a93238451313dab4eb0d4c53bb95019453
Dharan Aditya <dharan.aditya@outlook.com>
2024-01-17 13:57:29 +0530

    feat: in-memory storage to KvStore

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -2,20 +2,23 @@ use clap::Parser;
 use kvs::KvStore;
 use kvs::cli::{Cli, Command};
 fn main() {
-    let mut kvs = KvStore::new();
+    let mut _kvs = KvStore::new();
     let cli = Cli::parse();
     match cli.command {
         Some(Command::Get(args)) => {
-            kvs.get(args.key);
+            // kvs.get(args.key);
+            panic!("unimplemented")
         }
         Some(Command::Set(args)) => {
-            kvs.set(args.key, args.value);
+            // kvs.set(args.key, args.value);
+            panic!("unimplemented")
         }
         Some(Command::Remove(args)) => {
-            kvs.remove(args.key)
+            // kvs.remove(args.key)
+            panic!("unimplemented")
         }
         None => {
-            unimplemented!()
+            panic!("unimplemented")
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,34 @@
+use std::collections::HashMap;
+
 pub mod cli;
 
 pub struct KvStore {
-
+    store: HashMap<String, String>
 }
 
 impl KvStore {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            store: HashMap::new()
+        }
     }
 
     pub fn set(&mut self, key: String, value: String) {
-        panic!("unimplemented")
+        self.store.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        panic!("unimplemented")
+        match self.store.get(&key) {
+            Some(val) => {
+                Option::Some(val.to_owned())
+            }
+            None => {
+                Option::None
+            }
+        }
     }
 
     pub fn remove(&mut self, key: String) {
-        panic!("unimplemented")
+        let _ = &self.store.remove(&key);
     }
 }
