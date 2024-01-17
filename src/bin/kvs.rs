2b025d33b92358063b376870d2e3a6527a209d67
Dharan Aditya <dharan.aditya@outlook.com>
2024-01-17 14:15:53 +0530

    refactor code

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,19 +1,19 @@
 use clap::Parser;
 use kvs::KvStore;
-use kvs::cli::{Cli, Command};
+use kvs::{Cli, Command};
 fn main() {
     let mut _kvs = KvStore::new();
     let cli = Cli::parse();
     match cli.command {
-        Some(Command::Get(args)) => {
+        Some(Command::Get(_args)) => {
             // kvs.get(args.key);
             panic!("unimplemented")
         }
-        Some(Command::Set(args)) => {
+        Some(Command::Set(_args)) => {
             // kvs.set(args.key, args.value);
             panic!("unimplemented")
         }
-        Some(Command::Remove(args)) => {
+        Some(Command::Remove(_args)) => {
             // kvs.remove(args.key)
             panic!("unimplemented")
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,8 @@
-use std::collections::HashMap;
+#![deny(missing_docs)]
+//! A key-value store library
 
-pub mod cli;
-/// Key-value store implementation.
-pub struct KvStore {
-    /// The store.
-    store: HashMap<String, String>
-}
+mod kv;
+mod cli;
 
-impl KvStore {
-    /// Creates a new instance of KvStore.
-    pub fn new() -> Self {
-        Self {
-            store: HashMap::new()
-        }
-    }
-
-    /// Sets a key-value pair in the store.
-    ///
-    /// # Arguments
-    ///
-    /// * `key` - The key.
-    /// * `value` - The value.
-    pub fn set(&mut self, key: String, value: String) {
-        self.store.insert(key, value);
-    }
-
-    /// Retrieves the value associated with the given key from the store.
-    ///
-    /// # Arguments
-    ///
-    /// * `key` - The key.
-    ///
-    /// # Returns
-    ///
-    /// The value associated with the key, if it exists.
-    pub fn get(&self, key: String) -> Option<String> {
-        match self.store.get(&key) {
-            Some(val) => {
-                Option::Some(val.to_owned())
-            }
-            None => {
-                Option::None
-            }
-        }
-    }
-
-    /// Removes the key-value pair associated with the given key from the store.
-    ///
-    /// # Arguments
-    ///
-    /// * `key` - The key.
-    pub fn remove(&mut self, key: String) {
-        let _ = &self.store.remove(&key);
-    }
-}
+pub use kv::KvStore;
+pub use cli::{Cli, Command};
