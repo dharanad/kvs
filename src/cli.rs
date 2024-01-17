d5f0be70f17b79fce81bf9e730d6da9cfa9f1cc3
Dharan Aditya <dharan.aditya@outlook.com>
2024-01-17 14:10:37 +0530

    add: documentation

--- src/cli.rs
+++ src/cli.rs
@@ -1,32 +1,41 @@
 use clap::{Args, Parser, Subcommand};
 
+/// Command line interface struct.
 #[derive(Parser)]
 #[command(author, version, about)]
 pub struct Cli {
+    /// The command to run.
     #[command(subcommand)]
     pub command: Option<Command>
 }
-
+/// Enum representing the possible commands.
 #[derive(Subcommand)]
 pub enum Command {
-    // gets the value of a given key
+    /// Gets the value of a given key
     Get(GetArgs),
-    // sets the value of a key
+    /// Sets the value of a key
     Set(SetArgs),
-    // removes a given key
+    /// Removes a given key
     #[clap(name = "rm")]
     Remove(RemoveArgs)
 }
+/// Struct representing the arguments for the get command.
 #[derive(Args)]
 pub struct GetArgs {
+    /// The key.
     pub key: String
 }
+/// Struct representing the arguments for the set command.
 #[derive(Args)]
 pub struct SetArgs {
+    /// The key.
     pub key: String,
+    /// The value.
     pub value: String
 }
+/// Struct representing the arguments for the remove command.
 #[derive(Args)]
 pub struct RemoveArgs {
+    /// The key.
     pub key: String
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,39 @@
 use std::collections::HashMap;
 
 pub mod cli;
-
+/// Key-value store implementation.
 pub struct KvStore {
+    /// The store.
     store: HashMap<String, String>
 }
 
 impl KvStore {
+    /// Creates a new instance of KvStore.
     pub fn new() -> Self {
         Self {
             store: HashMap::new()
         }
     }
 
+    /// Sets a key-value pair in the store.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key.
+    /// * `value` - The value.
     pub fn set(&mut self, key: String, value: String) {
         self.store.insert(key, value);
     }
 
+    /// Retrieves the value associated with the given key from the store.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key.
+    ///
+    /// # Returns
+    ///
+    /// The value associated with the key, if it exists.
     pub fn get(&self, key: String) -> Option<String> {
         match self.store.get(&key) {
             Some(val) => {
@@ -28,6 +45,11 @@ impl KvStore {
         }
     }
 
+    /// Removes the key-value pair associated with the given key from the store.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key.
     pub fn remove(&mut self, key: String) {
         let _ = &self.store.remove(&key);
     }
