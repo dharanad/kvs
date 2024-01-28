d8f4398eec25997df8b60852e624a7fb034211b8
Dharan Aditya <dharan.aditya@outlook.com>
2024-01-28 21:55:03 +0530

    fix: LogEntry docs

--- src/log_entry.rs
+++ src/log_entry.rs
@@ -2,13 +2,9 @@ use bincode::{Decode, Encode};
 
 /*
 * LogEntry is the basic unit of the log.
-* Max Value Size : 256KB -> 262144 Bytes
-* Max Key Size : 64 Bytes
-* Max Log Entry Size : 262208 Bytes
 * Log Entry Format :
 * ksz | key | vsz | value
 * u64 | vec<u8> | u64 | vec<u8>
-* u64 -> 8 bytes
 */
 #[derive(Debug, Encode, Decode, Clone)]
 pub struct LogEntry {
